a53971642aef34978687167b0026bbd1722d7fd8
MEGABYTE <yashygkf@gmail.com>
2021-06-16 22:00:34 +0530

    maintaing provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/products_data_provider.dart';
 import './screens/product_detail_screen.dart';
 import './screens/product_overview_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -8,18 +11,20 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.amber,
-        fontFamily: 'Anton',
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsDataProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.amber,
+          fontFamily: 'Anton',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx)=> ProductDetailScreen(),
-      },
     );
   }
 }
-
