3802d02a212f4e4f0f846e19acd123942dd793b7
MEGABYTE <yashygkf@gmail.com>
2021-06-16 21:58:54 +0530

    debugging

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import './product_overview_screen.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product_detail_screen';
 
   @override
   Widget build(BuildContext context) {
-    final productId= ModalRoute.of(context)!.settings.arguments as String;
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
       appBar: AppBar(
         title: Text('title'),
