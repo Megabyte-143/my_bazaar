0e58b575844f1a0074058f5edce6dd61b9a4094c
MEGABYTE <yashygkf@gmail.com>
2021-06-16 17:55:23 +0530

    adding product's detail screen

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import './product_overview_screen.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  static const routeName = '/product_detail_screen';
+
+  @override
+  Widget build(BuildContext context) {
+    final productId= ModalRoute.of(context)!.settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+    );
+  }
+}
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,38 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:my_bazaar/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
-  final String description;
+
   final String imageUrl;
-  final double price;
 
   ProductItem(
     this.id,
     this.title,
-    this.description,
     this.imageUrl,
-    this.price,
   );
 
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child: Card(
-        child: Image.network(imageUrl),
-      ),
-      footer: GridTileBar(
-        backgroundColor: Colors.black54,
-        leading: IconButton(
-          icon: Icon(Icons.favorite),
-          onPressed: () {},
-        ),
-        title: Text(title),
-        trailing: IconButton(
-          icon: Icon(
-            Icons.shopping_bag,
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context)
+                .pushNamed(ProductDetailScreen.routeName, arguments: id);
+          },
+          child: Card(
+            child: Image.network(
+              imageUrl,
+              fit: BoxFit.cover,
+            ),
           ),
-          onPressed: () {},
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          title: Text(title),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_bag,
+            ),
+            onPressed: () {},
+          ),
         ),
       ),
     );
