c9372aa7ff922046861236020660c57fda0bbfda
Megabyte <yashygkf@gmail.com>
2021-06-17 03:28:35 +0530

    Reformating

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import './providers/products_data_provider.dart';
-import './screens/product_detail_screen.dart';
-import './screens/product_overview_screen.dart';
 
+import './providers/products_data_provider.dart';
+import './screens/product_overview_screen.dart';
+import './screens/product_detail_screen.dart';
 void main() {
   runApp(MyApp());
 }
--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../widgets/product_gird.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
