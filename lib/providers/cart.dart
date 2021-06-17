2ff0ed1ecd286cc2a6c681346dad67ef08323aad
Megabyte <yashygkf@gmail.com>
2021-06-17 17:01:39 +0530

    added cart

---
+++ lib/providers/cart.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class CartItem {
+  final String id;
+  final String title;
+  final double price;
+  final int quantity;
+
+  CartItem({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.quantity,
+  });
+}
+
+class Cart with ChangeNotifier {
+  late Map<String, CartItem> _items;
+  Map<String, CartItem> get items {
+    return {..._items};
+  }
+
+  void addItem(String productId, double price, String title) {
+    if (_items.containsKey(productId)) {
+      _items.update(
+          productId,
+          (existingValue) => CartItem(
+                id: existingValue.id,
+                title: existingValue.title,
+                price: existingValue.price,
+                quantity: existingValue.quantity + 1,
+              ));
+    } else {
+      _items.putIfAbsent(
+          productId,
+          () => CartItem(
+                id: DateTime.now().toString(),
+                title: title,
+                price: price,
+                quantity: 1,
+              ));
+    }
+  }
+}
