eae626cd5e0a77ec14c6f282351715ce37e52893
Megabyte <yashygkf@gmail.com>
2021-06-24 17:09:03 +0530

    added auth screen

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import './screens/cart_screen.dart';
 import './screens/order_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
 void main() {
   runApp(MyApp());
 }
@@ -36,7 +37,7 @@ class MyApp extends StatelessWidget {
           accentColor: Colors.amber,
           fontFamily: 'Anton',
         ),
-        home: ProductOverviewScreen(),
+        home: AuthScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
