b28a710f4441a2632329906aa5fcf6a12f49150f
Megabyte <yashygkf@gmail.com>
2021-07-06 04:38:35 +0530

    added custom routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import './providers/cart_data_provider.dart'; 
+import './providers/cart_data_provider.dart';
 import './providers/products_data_provider.dart';
 import './providers/orders_data_provider.dart';
 import './providers/auth_provider.dart';
@@ -14,6 +14,7 @@ import './screens/order_screen.dart';
 import './screens/cart_screen.dart';
 import './screens/auth_screen.dart';
 import './screens/splash_screen.dart';
+import './helpers/custom_route.dart';
 
 void main() {
   runApp(MyApp());
@@ -51,10 +52,14 @@ class MyApp extends StatelessWidget {
         builder: (context, auth, _) => MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
-            primarySwatch: Colors.purple,
-            accentColor: Colors.amber,
-            fontFamily: 'Anton',
-          ),
+              primarySwatch: Colors.purple,
+              accentColor: Colors.amber,
+              fontFamily: 'Anton',
+              pageTransitionsTheme: PageTransitionsTheme(
+                builders: {
+                  TargetPlatform.android:CustomPageTransitionBuilder(),
+                },
+              )),
           home: auth.isAuth
               ? ProductOverviewScreen()
               : FutureBuilder(
